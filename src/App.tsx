4fe05a3a0b11f51dfc6b3507a535c814fec5870e
SergFediaev <sergfediaev@gmail.com>
2025-04-19 13:03:13 +0300

    feat: компонент кнопки из библиотеки

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from 'sat-ui'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Button>Button in front project from local ui-kit</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
